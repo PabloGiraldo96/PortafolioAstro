4898186f944893af4c5c4c548f9169320e3c3f6b
Juan Pablo Jaramillo Giraldo <juanpablogiraldo9602@gmail.com>
2024-04-13 21:34:32 -0500

    se añade proyecto

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+import { defineConfig } from 'astro/config';
+import vue from "@astrojs/vue";
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  devToolbar: {
+    enabled: false
+  },
+  integrations: [vue(), tailwind()]
+});
---
+++ src/components/Example.vue
@@ -0,0 +1,161 @@
+<script setup lang="ts">
+
+import { TresCanvas, useRenderLoop } from '@tresjs/core'
+import { BasicShadowMap, SRGBColorSpace, NoToneMapping } from 'three'
+import { OrbitControls, Stars } from '@tresjs/cientos'
+import { shallowRef, watchEffect } from 'vue';
+
+
+const gl = {
+  clearColor: '#09070D',
+  shadows: true,
+  alpha: false,
+  shadowMapType: BasicShadowMap,
+  outputColorSpace: SRGBColorSpace,
+  toneMapping: NoToneMapping,
+}
+
+const {onLoop} = useRenderLoop()
+
+// ShallowRef de cada geometria, como cada geometria tiene diferentes animaciones, las voy a manejar por separado, 6 aproximadamente
+
+const geometryRef = shallowRef()
+const geometryRefTwo = shallowRef()
+const geometryRefThree = shallowRef()
+const geometryRefFour = shallowRef()
+const geometryRefFive = shallowRef()
+const geometryRefSix = shallowRef()
+
+onLoop(({delta, elapsed}) => {
+	if(geometryRef.value && geometryRefTwo.value && geometryRefThree.value && geometryRefFour.value&&geometryRefFive.value&&geometryRefSix.value){
+		//geometryRef.value.rotation.y += 0.05
+		//geometryRef.value.rotation.z = elapsed
+		geometryRef.value.position.set(0, 0, Math.sin(elapsed * -0.15) * 0.5);
+		//geometryRef.value.position.x = Math.sin(elapsed * 0.1) * 1
+		geometryRef.value.scale.set(
+			Math.sin(elapsed) * .15 -0.1,
+			Math.sin(elapsed) * .15 -0.1,
+			Math.sin(elapsed) * .15 -0.1,)
+
+// Segunda geometria
+
+		geometryRefTwo.value.rotation.y += 0.003
+		geometryRefTwo.value.rotation.x += 0.004
+		geometryRefTwo.value.rotation.z = elapsed
+		geometryRefTwo.value.position.y = Math.sin(-elapsed * 1.5) * 1
+		geometryRefTwo.value.position.x = Math.cos(elapsed * 0.1) * 1
+		geometryRefTwo.value.position.z = Math.sin(elapsed * 0.3) * 1
+		geometryRefTwo.value.scale.set(
+			Math.sin(elapsed) * .15 -0.1,
+			Math.sin(elapsed) * .15 -0.1,
+			Math.sin(elapsed) * .15 -0.1)
+
+// Tercer geometria 
+
+		geometryRefThree.value.rotation.y += 0.003
+		geometryRefThree.value.rotation.x += 0.004
+		geometryRefThree.value.rotation.z = elapsed
+		geometryRefThree.value.position.y = Math.sin(elapsed * -1.5) * -1
+		geometryRefThree.value.position.x = Math.cos(-elapsed * 0.1) * -1
+		geometryRefThree.value.position.z = Math.sin(elapsed * 0.3) * -1
+		geometryRefThree.value.scale.set(
+			Math.sin(elapsed) * .15 -0.1,
+			Math.sin(elapsed) * .15 -0.1,
+			Math.sin(elapsed) * .15 -0.1)
+
+// Cuarta geometria 
+
+	geometryRefFour.value.position.set(0, 0, Math.sin(elapsed * 1.15) * 0.75);
+		geometryRefFour.value.scale.set(
+			Math.sin(elapsed) * .15 -0.1,
+			Math.sin(elapsed) * .15 -0.1,
+			Math.sin(elapsed) * .15 -0.1)
+
+  // Quinta geometria
+
+	geometryRefFive.value.position.set(1, 0, Math.sin(elapsed * 1.15) * 0.75);
+		geometryRefFive.value.scale.set(
+			Math.sin(elapsed) * .15 -0.1,
+			Math.sin(elapsed) * .15 -0.1,
+			Math.sin(elapsed) * .15 -0.1)
+
+// Sexta geometria
+
+	geometryRefSix.value.position.set(-1, 0, Math.sin(elapsed * 1.15) * 0.75);
+		geometryRefSix.value.scale.set(
+			Math.sin(elapsed) * .15 -0.1,
+			Math.sin(elapsed) * .15 -0.1,
+			Math.sin(elapsed) * .15 -0.1)
+
+	}
+})
+
+</script>
+<template>
+    <div class="tres-container">
+    <TresCanvas v-bind="gl">
+    <TresPerspectiveCamera :position="[1,1,1]" :look-at="[0,-2,0]" />
+    <OrbitControls />
+    <Stars />
+    <Suspense>
+      <TresMesh ref="geometryRef" transition:animate="slide">
+		  <TresBoxGeometry :args="[1, 1, 1, 1]" />  
+          <TresMeshNormalMaterial />     
+      </TresMesh>
+    </Suspense>
+    <Suspense>
+    <TresMesh ref="geometryRefTwo">
+      <TresBoxGeometry :args="[2, 1, 1, 1]" />
+      <TresMeshNormalMaterial />
+    </TresMesh>
+    </Suspense>
+    <Suspense>
+    <TresMesh ref="geometryRefThree">
+      <TresBoxGeometry :args="[2, 1, 1, 1]" />
+      <TresMeshNormalMaterial />
+    </TresMesh>
+    </Suspense>
+    <Suspense>
+    <TresMesh ref="geometryRefFour">
+      <TresBoxGeometry :args="[1, 1, 1, 1]" />
+      <TresMeshNormalMaterial />
+    </TresMesh>
+    </Suspense>
+    <Suspense>
+    <TresMesh ref="geometryRefFive">
+      <TresBoxGeometry :args="[1, 1, 1, 1]" />
+      <TresMeshNormalMaterial />
+    </TresMesh>
+    </Suspense>
+    <Suspense>
+    <TresMesh ref="geometryRefSix">
+      <TresBoxGeometry :args="[1, 1, 1, 1]" />
+      <TresMeshNormalMaterial />
+    </TresMesh>
+    </Suspense>
+    <TresAmbientLight :intensity="2" />
+    <TresDirectionalLight
+      :position="[-4, -2, 2]"
+      :intensity="1"
+      cast-shadow
+      color="#fff"
+    />
+    <TresDirectionalLight
+      :position="[4, 6, 4]"
+      :intensity="2"
+      cast-shadow
+      color="white"
+    />
+  </TresCanvas>
+  </div>
+</template>
+
+<style>
+.tres-container {
+  display: flex;
+  margin-bottom: -245px;
+  z-index: 1;
+  width: 100%;
+  height: 50vh;
+}
+</style>
